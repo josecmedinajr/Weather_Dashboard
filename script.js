e0e9bd0a3811dcef7ffea28b3e2cc282418869e0
Jose Medina <yanoya10@hotmail.com>
2020-02-27 19:01:32 -0500

    added search box functionality, v2 has arrived

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ var update = function() {
 }
 setInterval(update, 1000);
 
-// JQuery getJSON function to get data from openweathermap.org (Miami) with my API: 3cdcca706cc22e516be1f657eb0929a4
-var city = "Miami"
+// JQuery getJSON function to get data from openweathermap.org with my API: 3cdcca706cc22e516be1f657eb0929a4
+
+$(document).ready(function() {
+    $("#search").click(function() {
+      console.log($("#city").val());
+
+      var city = $("#city").val();
 
 $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=3cdcca706cc22e516be1f657eb0929a4",function(data){
-    console.log(data);
+console.log(data);
 
     var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
     var temperature = Math.floor(data.main.temp);
@@ -18,4 +23,10 @@ $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=
     $(".weather-icon").attr("src", icon);
     $(".temperature").append(temperature);
     $(".weather-condition").append(weatherCondition);
+    
+    });
+
+  });
+
+
 });
